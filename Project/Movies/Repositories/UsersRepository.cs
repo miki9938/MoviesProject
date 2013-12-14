5cec60bf6d1309dc419d992f498871a7f6feca62
kebabkiller <ad.nikodem@gmail.com>
2013-12-14 15:17:27 +0100

    Dodano folder z repozytoriami
    Podstawowe 2 repozytoria, jedynie z konstruktorami

---
+++ Project/Movies/Repositories/UsersRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Repositories
+{
+    public class UsersRepository
+    {
+        MoviesEntities db;
+
+        UsersRepository()
+        {
+            db = new MoviesEntities();
+        }
+    }
+}
