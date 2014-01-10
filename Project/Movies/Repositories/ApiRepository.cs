600231077709e68905d012246d77db797c81e70c
kebabkiller <ad.nikodem@gmail.com>
2014-01-10 23:08:15 +0100

    Baza V2 + repozytorium do api

---
+++ Project/Movies/Repositories/ApiRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Movies.Repositories
+{
+    public class ApiRepository
+    {
+        MoviesEntities db;
+
+
+        public ApiRepository()
+        {
+            db = new MoviesEntities();
+        }
+
+        public XDocument getXmlByMovieId(int id)
+        { 
+            
+            movie temp = db.movie.Where(a => a.id.Equals(id)).FirstOrDefault();
+
+            XDocument xmlTemp = new XDocument(
+                new XElement("Title", temp.title),
+                new XElement("Plot", temp.description),
+                new XElement("Date", temp.release_date),
+                new XElement("Directors", temp.cast.Where(a => a.role.Equals(1))),
+                new XElement("Writers", temp.cast.Where(a => a.role.Equals(3))),
+                new XElement("Actors", temp.cast.Where(a => a.role.Equals(2)))
+                                            );
+
+            return xmlTemp; 
+        }
+    }
+}
