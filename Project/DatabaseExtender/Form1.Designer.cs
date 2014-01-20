5ba05846001f0944997d328fa2755ad4fdea02d8
kebabkiller <ad.nikodem@gmail.com>
2014-01-20 20:22:34 +0100

    DataBaseExtender update
    obsługa omdbapi.com
    Zamień connection string'a Movies w
    project\databaseExtender\App.config

--- Project/DatabaseExtender/Form1.Designer.cs
+++ Project/DatabaseExtender/Form1.Designer.cs
@@ -31,18 +31,20 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(102, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 12);
+            this.button1.Location = new System.Drawing.Point(119, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 46);
             this.button1.TabIndex = 1;
@@ -52,17 +54,41 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 38);
+            this.textBox2.Location = new System.Drawing.Point(12, 57);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(101, 20);
             this.textBox2.TabIndex = 2;
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 8);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "omdbapi.com";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(119, 8);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "mymovieapi.com";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(243, 72);
+            this.ClientSize = new System.Drawing.Size(243, 89);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -78,6 +104,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- Project/DatabaseExtender/Form1.cs
+++ Project/DatabaseExtender/Form1.cs
@@ -17,6 +17,8 @@ namespace DatabaseExtender
         string url;
         string urlParams;
 
+        enum months { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+
         public Form1()
         {
 
@@ -28,97 +30,222 @@ namespace DatabaseExtender
 
         private void button1_Click(object sender, EventArgs e)
         {
-            JObject data;
-            movie tempMovie;
-            person tempPerson;
+            if (radioButton1.Checked)
+                getDataTwo();
+            else
+                getDataOne();
+        }
 
-            url = "http://mymovieapi.com/?id=";
-            url += textBox1.Text + urlParams;
+        private void getDataOne()
+        {
+            try
+            {
+                JObject data;
+                movie tempMovie;
+                person tempPerson;
 
-            tempMovie = new movie();
-            tempPerson = new person();
+                url = "http://mymovieapi.com/?id=";
+                url += textBox1.Text + urlParams;
 
-            WebRequest request = WebRequest.Create(url);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                tempMovie = new movie();
+                tempPerson = new person();
 
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
+                WebRequest request = WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            data = JObject.Parse(reader.ReadToEnd());
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
 
-            int year = (int)data["release_date"][0]["year"];
-            int day = (int)data["release_date"][0]["day"];
-            int month = (int)data["release_date"][0]["month"];
+                data = JObject.Parse(reader.ReadToEnd());
 
-            DateTime date = new DateTime(year, month, day);
+                int year = (int)data["release_date"][0]["year"];
+                int day = (int)data["release_date"][0]["day"];
+                int month = (int)data["release_date"][0]["month"];
 
-            tempMovie.title = ((string)data["title"]).ToLower();
-            tempMovie.release_date = date;
-            tempMovie.description = (string)data["plot"];
+                DateTime date = new DateTime(year, month, day);
+
+                tempMovie.title = ((string)data["title"]).ToLower();
+                tempMovie.release_date = date;
+                tempMovie.description = (string)data["plot"];
+
+                bool c = dbMovie.addMovie(tempMovie);
+
+                textBox2.Text = c.ToString();
+
+                cast tempCast = new cast();
+                tempCast.movie_id = tempMovie.id;
+
+                char[] separator = { ' ' };
+
+                tempMovie.id = (dbMovie.getMovieByTitle(tempMovie.title)).id;
+
+                foreach (string a in data["actors"])
+                {
+                    tempPerson.name = a.ToLower();
+
+                    dbPerson.addNewPerson(tempPerson);
+
+
+                }
+
+                foreach (string d in data["directors"])
+                {
+                    tempPerson.name = d.ToLower();
+
+                    dbPerson.addNewPerson(tempPerson);
+                }
+
+                foreach (string w in data["writers"])
+                {
+                    tempPerson.name = w.ToLower();
+
+                    dbPerson.addNewPerson(tempPerson);
+                }
+
+                foreach (string a2 in data["actors"])
+                {
+
+                    tempCast.person_id = (dbPerson.getPersonByName(a2)).id;
+                    tempCast.role = 2;
+                    dbPerson.addRole(tempCast);
+                }
+
+                foreach (string d2 in data["directors"])
+                {
+
+                    tempCast.person_id = (dbPerson.getPersonByName(d2)).id;
+                    tempCast.role = 1;
+                    dbPerson.addRole(tempCast);
+                }
+
+                foreach (string w2 in data["writers"])
+                {
+
+                    tempCast.person_id = (dbPerson.getPersonByName(w2)).id;
+                    tempCast.role = 3;
+                    dbPerson.addRole(tempCast);
+                }
+
+                dataStream.Close();
+                reader.Close();
+                response.Close();
+
+                textBox1.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("Nie można połączyć z serwerem");
+            }
+        }
+
+        private void getDataTwo()
+        {
+            try
+            {
+                JObject data;
+                movie tempMovie;
+                person tempPerson;
+
+                url = "http://www.omdbapi.com/?i=";
+                url += textBox1.Text;
+
+                tempMovie = new movie();
+                tempPerson = new person();
+
+                WebRequest request = WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+
+                data = JObject.Parse(reader.ReadToEnd());
+
+                string[] dateTemp = Convert.ToString(data["Released"]).Split(' ');
                 
-            bool c = dbMovie.addMovie(tempMovie);
+                int tempMonth = (int)Enum.Parse(typeof(months), dateTemp[1]);
 
-            textBox2.Text = c.ToString();
-            
-            cast tempCast = new cast();
-            tempCast.movie_id = tempMovie.id;
+                DateTime date = new DateTime(Convert.ToInt32(dateTemp[2]), tempMonth, Convert.ToInt32(dateTemp[0]));
 
-            char[] separator = { ' ' };
+                tempMovie.title = ((string)data["Title"]).ToLower();
+                tempMovie.release_date = date;
+                tempMovie.description = (string)data["Plot"];
 
-            tempMovie.id = (dbMovie.getMovieByTitle(tempMovie.title)).id;
+                bool c = dbMovie.addMovie(tempMovie);
 
-            foreach (string a in data["actors"])
-            {
-                tempPerson.name = a.ToLower();
+                textBox2.Text = c.ToString();
 
-                dbPerson.addNewPerson(tempPerson);
-                
+                cast tempCast = new cast();
+                tempCast.movie_id = tempMovie.id;
 
+                char[] separator = { ' ' };
+
+                tempMovie.id = (dbMovie.getMovieByTitle(tempMovie.title)).id;
+
+                string[] Actors = Convert.ToString(data["Actors"]).Split(',');
+                string[] Writers = Convert.ToString(data["Writer"]).Split(',');
+                string[] Directors = Convert.ToString(data["Director"]).Split(',');
+
+                foreach (string a in Actors)
+                {
+                    tempPerson.name = a.ToLower();
+
+                    dbPerson.addNewPerson(tempPerson);
+
+
+                }
+
+                foreach (string d in Directors)
+                {
+                    tempPerson.name = d.ToLower();
+
+                    dbPerson.addNewPerson(tempPerson);
+
+                }
+
+                foreach (string w in Writers)
+                {
+                    string[] tempName = w.ToLower()
+                                         .Split('(');
+
+                    tempPerson.name = tempName[0];
+                    dbPerson.addNewPerson(tempPerson);
+                }
+
+                foreach (string a2 in Actors)
+                {
+
+                    tempCast.person_id = (dbPerson.getPersonByName(a2)).id;
+                    tempCast.role = 2;
+                    dbPerson.addRole(tempCast);
+                }
+
+                foreach (string d2 in Directors)
+                {
+
+                    tempCast.person_id = (dbPerson.getPersonByName(d2)).id;
+                    tempCast.role = 1;
+                    dbPerson.addRole(tempCast);
+                }
+
+                foreach (string w2 in Writers)
+                {
+                    string[] tempName = w2.ToLower()
+                                         .Split('(');
+                    tempCast.person_id = (dbPerson.getPersonByName(tempName[0])).id;
+                    tempCast.role = 3;
+                    dbPerson.addRole(tempCast);
+                }
+
+                dataStream.Close();
+                reader.Close();
+                response.Close();
+
+                textBox1.Clear();
             }
-
-            foreach (string d in data["directors"])
+            catch
             {
-                tempPerson.name = d.ToLower();
-
-                dbPerson.addNewPerson(tempPerson);
-            }
-
-            foreach (string w in data["writers"])
-            {
-                tempPerson.name = w.ToLower();
-
-                dbPerson.addNewPerson(tempPerson);
-            }
-
-            foreach (string a2 in data["actors"])
-            {
-
-                tempCast.person_id = (dbPerson.getPersonByName(a2)).id;
-                tempCast.role = 2;
-                dbPerson.addRole(tempCast);
-            }
-
-            foreach (string d2 in data["directors"])
-            {
-
-                tempCast.person_id = (dbPerson.getPersonByName(d2)).id;
-                tempCast.role = 1;
-                dbPerson.addRole(tempCast);
-            }
-
-            foreach (string w2 in data["writers"])
-            {
-
-                tempCast.person_id = (dbPerson.getPersonByName(w2)).id;
-                tempCast.role = 3;
-                dbPerson.addRole(tempCast);
-            }
-
-            dataStream.Close();
-            reader.Close();
-            response.Close();
-
-            textBox1.Clear();
+                MessageBox.Show("Nie można połączyć z serwerem");
+            }            
         }
     }
 }
